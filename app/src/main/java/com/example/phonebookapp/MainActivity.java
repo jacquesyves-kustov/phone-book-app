fca38b8a665c9647ebe3ffb44a90d812e1381d23
Sergei Kustov <96955467+jacquesyves-kustov@users.noreply.github.com>
2022-05-21 20:47:52 +0500

    Определен базовый UI, структурирован MainActivity.xml и подготовлены переменные в MainActivity.java

--- app/src/main/java/com/example/phonebookapp/MainActivity.java
+++ app/src/main/java/com/example/phonebookapp/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.phonebookapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.example.phonebookapp.databinding.ActivityMainBinding;
@@ -23,9 +26,24 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        // Example of a call to a native method
-        TextView tv = binding.sampleText;
-        tv.setText(stringFromJNI());
+        // UI elements
+        // Buttons
+        Button btShowAll = findViewById(R.id.showAllButton);
+        Button btClear = findViewById(R.id.clearButton);
+        Button btFindContact = findViewById(R.id.findButton);
+        Button btAddContact = findViewById(R.id.addButton);
+        Button btDebug = findViewById(R.id.debugButton);
+
+        // Other
+        ListView contactsList = findViewById(R.id.contactView);
+        EditText searchField = findViewById(R.id.searchEditText);
+        EditText newNameField = findViewById(R.id.newNameEditText);
+        EditText newNumberField = findViewById(R.id.newNumberEditText);
+        TextView totalContacts = binding.contactsCounter;
+
+        // Set default value to 'totalContacts'
+        totalContacts.setText("Всего контактов: 0");
+
     }
 
     /**
